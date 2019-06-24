8ae0807fe451b85dcd12f5e62d220e83016ce727
Eduardo Aguillar Cerqueira <eduardo.cerqueira@programmers.com.br>
2019-06-24 14:02:42 -0300

    added db.json and rent-a-movie node server

---
+++ rent-a-movie-server.js
@@ -0,0 +1,22 @@
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.db = router.db;
+
+const rules = auth.rewriter({
+  // Permission rules
+  rentals: 660
+})
+
+server.use(middlewares);
+server.use(rules);
+server.use(auth);
+server.use(router);
+
+server.listen(3000, () => {
+  console.log('rent-a-movie server is running');
+})
