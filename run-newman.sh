3f63746b6b9a017ab43270e1402f7b47769fe13b
Eduardo Aguillar Cerqueira <eduardo.cerqueira@programmers.com.br>
2019-07-02 09:23:54 -0300

    modified globals file so there's only one available instead of two

--- run-newman.sh
+++ run-newman.sh
@@ -2,4 +2,4 @@
 
 docker pull postman/newman:4.5-alpine
 
-docker run -v c:/development/rent-a-movie/postman/test-suite:/etc/newman -t postman/newman:4.5-alpine run "./collections/End to End Tests.postman_collection.json" --environment="./environments/Docker Env.postman_environment.json" --globals="./globals/DockerEnv.postman_globals.json" --reporters junit --reporter-junit-export="newman-report.xml"
+docker run -v c:/development/rent-a-movie/postman/test-suite:/etc/newman -t postman/newman:4.5-alpine run "./collections/End to End Tests.postman_collection.json" --environment="./environments/Docker Env.postman_environment.json" --globals="./globals/ACELERA - Rent a Movie.postman_globals.json" --reporters junit --reporter-junit-export="newman-report.xml"
